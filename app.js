a0768dd286fddacec6c423908c22c4d64f0a7bd0
Victor Tesseroli <victortesseroli2@gmail.com>
2025-01-13 23:46:39 -0300

    first commit

--- app.js
+++ app.js
@@ -2,14 +2,28 @@
 express = require('express')
 app = express()
 router = require('./routes/passageiroRoutes')
-app.use(express.json());
-
+require('dotenv').config()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const cors = require('cors');
 
 app.use(cors());
+app.use(express.json());
 
 const port = 3000
 app.use(router)
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'bem vindo a api' })
+})
+
+app.post('/auth/register', async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body
+
+    if(!name){
+        return res.status(422).json({msg: 'O nome é obrigatório!'})
+    }
+})
 
 console.log('o server esta online na porta ' + port)
 app.listen(port)
--- db/cnx.js
+++ db/cnx.js
@@ -1,8 +1,12 @@
 const { Pool } = require('pg')
+require('dotenv').config();
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
 
 const poll = new Pool({
-    user: 'postgres',
-    password: 'postgres',
+    user: dbUser,
+    password: dbPassword,
     host: 'localhost',
     port: 5432,
     database: 'dbFaculRideCerto'
